d956c0710bbaa9bc664bf2c039a4e6f069d35ccd
Kimgwangil <kwangil0803@gmail.com>
2022-04-05 22:48:42 +0900

    백엔드의 로그인구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,43 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render("home/index")
-};
 
-
-const login = (req, res) => {
-  res.render("home/login")
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  }
 }
 
+const users = {
+  id: ["kgi", "나참눼", "김팀장"],
+  password: ["1234", "12345", "123456"]
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      password = req.body.password;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.password[idx] === password) {
+        return res.json({
+          success: true,
+        });
+      }
+    
+      return res.json({
+        success: false,
+        msg: "로그인에 실패하였습니다."
+      })
+    }
+  }
+}
+
+
 module.exports = {
-  home,
-  login,
+  output,
+  process
 };
